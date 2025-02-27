6911255112d4b7368825b72f1d12864174ba2a47
Eldar Gurbanov <mequrbanov@gmail.com>
2025-02-28 01:03:52 +0400

    run.sh command is now with --json-stream option

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # docker run --rm -it --net=monitoring --name iperf3_server monitoring-telegraf:latest /usr/bin/iperf3 -s
-./src/iperf3 -c iperf3_server -t 2 -i 1 -u -b 15m -R -J
+./src/iperf3 -c iperf3_server -t 10 -i 2 -u -b 15m -R -J --json-stream
